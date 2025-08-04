af02f40aba9cf0d11e16ddc22f5361334ed2ca41
Larryyang727 <88527340+Larryyang727@users.noreply.github.com>
2025-08-04 14:05:01 +0800

    Update data_utils.py

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,13 +1,13 @@
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, random_split
-
+#套用IMAGENET訓練資料集的標準差和平均值
 mean = [0.485, 0.456, 0.406]
 std = [0.229, 0.224, 0.225]
 
 def get_transforms():
     train_transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.RandomHorizontalFlip(),
+        transforms.Resize((224, 224)), #大多模型預設輸入224
+        transforms.RandomHorizontalFlip(), #隨機翻轉
         transforms.ToTensor(),
         transforms.Normalize(mean, std),
     ])
@@ -26,7 +26,7 @@ def get_dataloaders(data_dir, batch_size=32, val_ratio=0.2):
     val_len = len(full_dataset) - train_len
     train_dataset, val_dataset = random_split(full_dataset, [train_len, val_len])
     
-    # 分別套用不同 transform
+    #套用不同transform
     train_dataset.dataset.transform = train_transform
     val_dataset.dataset.transform = val_transform
     
@@ -34,3 +34,4 @@ def get_dataloaders(data_dir, batch_size=32, val_ratio=0.2):
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
     
     return train_loader, val_loader, full_dataset.class_to_idx
+
